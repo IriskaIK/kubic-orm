644636775f3819c275ff86df4bc293bee78ca60c
Misha <mixo228aga@gmail.com>
2024-09-25 21:06:33 +0300

    reworked RelationMapping interface, so that it will allow to add throughTable for ManyToMany Relations

--- src/types/model.types.ts
+++ src/types/model.types.ts
@@ -2,13 +2,19 @@ import {Relation} from "@/relations/Relation";
 import Model from "@/base-model/baseModel";
 
 export interface RelationMapping {
-    relation : typeof Relation,
-    model : typeof Model,
-    join : {
-        from : string,
-        to : string,
-        through? : string
-    }
+    relation: typeof Relation;
+    model: typeof Model;
+
+    join: {
+        from: string;      // source table
+        to: string;        // related table
+        throughTable?: {        // Optional for ManyToMany
+            // TODO: Decide whether its needed or not!
+         //   tableName: string;  // intermediary table
+            from: string;       // Foreign key in through table linking to source table
+            to: string;         // Foreign key in through table linking to related table
+        };
+    };
 }
 
 export interface RelationalMappings {
