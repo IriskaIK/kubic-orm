578134bc59f5a2630894d5cd194eebcda86cd29a
Misha <mixo228aga@gmail.com>
2024-09-25 16:17:19 +0300

    implemented ManyToMany Relation func.

--- src/relations/ManyToManyRelation/ManyToManyRelation.ts
+++ src/relations/ManyToManyRelation/ManyToManyRelation.ts
@@ -10,50 +10,21 @@ export class ManyToManyRelation<S, R extends Model> extends Relation<S, R>{
     }
 
 
-    // Table C has columns for both A and B
-    // tables' identifiers. This relationship is called
-    // ManyToManyRelation in objection. Each row in
-    // C joins one A with one B . Therefore, an A
-    // row can be related to multiple B rows and a
-    // B row can be related to multiple A rows
-    // through table С.
-    // Create INNER JOIN between A -> C and C -> B
     public createJoinClause(): Join[] {
-        const joins: Join[] = [];
-
-        // 1st Join: A (sourceModelClass) -> C (throughTable)
-        joins.push({
+        return [{
             type: 'INNER',
-            tables: [this.sourceModelClass.tableName, this.throughTable],
+            tables: [this.sourceModelClass.tableName, this.relatedModelClass.tableName],
             on: [{
                 leftColumn: {
-                    column: this.sourceIdentiferColumn,
+                    column: this.sourceIdentiferColumn, // Foreign key in A that references B
                     parentTable: this.sourceModelClass.tableName
                 },
                 rightColumn: {
-                    column: `${this.sourceModelClass.tableName}_id`,
-                    parentTable: this.throughTable
-                }
-            }]
-        });
-
-        // 2nd Join: C -> B (relatedModelClass)
-        joins.push({
-            type: 'INNER',
-            tables: [this.throughTable, this.relatedModelClass.tableName],
-            on: [{
-                leftColumn: {
-                    column: `${this.relatedModelClass.tableName}_id`,
-                    parentTable: this.throughTable
-                },
-                rightColumn: {
-                    column: this.relatedIdentiferColumn,
+                    column: this.relatedIdentiferColumn, // Foreign key in B that references A
                     parentTable: this.relatedModelClass.tableName
                 }
             }]
-        });
-
-        return joins;
+        }];
     }
 
 
